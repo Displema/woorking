c17ae428d6b716d331f42ddbcf5e3476138b5f75
displema <moraema@outlook.it>
2025-05-14 17:42:14 +0200

    add ramsey uuid, refactor EFoto to support doctrine

--- Entity/EFoto.php
+++ Entity/EFoto.php
@@ -1,50 +1,58 @@
 <?php
-class EFoto{
-    private $id;
-    private $data; //tipo blob rappresentato come string
-    private $filename;
-    private $extension;
 
-    public function __construct(int $id, string $data, string $filename, string $extension) {
-        $this->id = $id;
-        $this->data = $data; //dati binari dell'immagine
-        $this->filename = $filename;
-        $this->extension = $extension;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="foto")
+ */
+class EFoto{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="guid", unique=true)
+     */
+    private UuidInterface $id;
+    /** @ORM\Column(type="blob") */
+    private $content;
+
+    /** @ORM\Column(type="string") */
+    private $mimeType;
+
+    /** @ORM\Column(type="integer") */
+    private int $size;
+
+    public function __construct()
+    {
+        // Genera l'UUID (versione 4)
+        $this->id = Uuid::uuid4();  // La libreria Ramsey\Uuid crea un UUID v4
     }
 
-    public function getId(): int {
+    public function getId(): UuidInterface {
         return $this->id;
     }
 
-    public function getData(): string {
-        return $this->data;
+    public function getContent(): string {
+        return $this->content;
     }
 
-    public function getFilename(): string {
-        return $this->filename;
+    public function getMimeType(): string {
+        return $this->mimeType;
     }
 
-    public function getExtension(): string {
-        return $this->extension;
-    }
-
-    public function setId(int $id): void {
+    public function setId(UuidInterface $id): void {
         $this->id = $id;
     }
 
-    public function setData(string $data): void {
-        $this->data = $data;
+    public function setContent($content): void {
+        $this->content = $content;
     }
 
-    public function setFilename(string $filename): void {
-        $this->filename = $filename;
-    }
-
-    public function setExtension(string $extension): void {
-        $this->extension = $extension;
+    public function setMimetype(string $mimeType): void {
+        $this->mimeType = $mimeType;
     }
 
     public function __toString(): string {
-        return "EFoto(ID: $this->id, Filename: $this->filename, Extension: $this->extension)";
+        return "EFoto(ID: $this->id, mimeType: $this->mimeType, Size: $this->size)";
     }
 }
