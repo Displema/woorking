7fb8d4c77bdda3aa5a9762106518768010520cad
PasoPaso69 <andreapa744@gmail.com>
2025-05-23 12:54:10 +0200

    foundation and FPersistentManager

---
+++ TechnicalServiceLayer/FPersistentManager.php
@@ -0,0 +1,28 @@
+<?php
+
+class FPersistentManager{
+    // E' una classe singleton ovvero in tutto il progrmma esiste una sola istanza di questa classe alla quale si può accedere in maniera globale
+
+    private static $instance;
+
+    private function __construct(){
+        
+
+
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new self();
+        }
+        return self::$instance;}
+
+        public static function retriveobj($Eclass,$id){
+            $result=FEntityManager::getInstance()->retriveobj($Eclass,$id);
+            return $result; 
+        }
+         public static function uploadObj($obj){
+            $result=FEntityManager::getInstance()->saveObj($obj);
+            return $result;
+         }
+}
