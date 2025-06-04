aba76490f6d82568bf4ee3c4a9661730d0f6b008
displema <moraema@outlook.it>
2025-06-04 12:38:24 +0200

    update fixtures

--- Testing/Fixtures/EUfficioFixture.php
+++ Testing/Fixtures/EUfficioFixture.php
@@ -26,8 +26,8 @@ class EUfficioFixture extends AbstractFixture implements DependentFixtureInterfa
                 ->setLocatore($this->getReference('ELocatore_' . $i, ELocatore::class))
                 ->setPrezzo($faker->randomNumber(2, true))
                 ->setStato($faker->randomElement(StatoUfficioEnum::class))
-                ->setNumeroPostazioni($faker->randomNumber(1, true))
-                ->setSuperficie($faker->randomNumber(1, true))
+                ->setNumeroPostazioni($faker->randomNumber(2, true))
+                ->setSuperficie($faker->randomNumber(2, true))
                 ->setDataCaricamento($faker->dateTimeBetween("-80 days", "-61 days"))
                 ->setIndirizzo($this
                     ->getReference('EIndirizzo_' . $i, EIndirizzo::class));
