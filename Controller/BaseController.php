1a6a229e2f7e6b60e2890efb11c86347d1088ac8
displema <moraema@outlook.it>
2025-06-09 11:10:36 +0200

    add auth check function to basecontroller to avoid code duplication

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -4,6 +4,10 @@ namespace Controller;
 
 use Delight\Auth\Auth;
 use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
+use TechnicalServiceLayer\Roles\Roles;
+use View\VRedirect;
+use View\VStatus;
 
 abstract class BaseController
 {
@@ -14,5 +18,50 @@ abstract class BaseController
     {
         $this->entity_manager = getEntityManager();
         $this->auth_manager = getAuth();
+    }
+
+    public function requireLogin(): void
+    {
+        if (!$this->auth_manager->isLoggedIn()) {
+            $view = new VRedirect();
+            $view->redirect('/login');
+            exit;
+        }
+    }
+
+    public function requireRole(string $role): void
+    {
+        $validRoles = (new \ReflectionClass(Roles::class))->getConstants();
+
+        if (!in_array($role, $validRoles, true)) {
+            throw new InvalidArgumentException('Invalid role: ' . $role . 'passed to requireRole()');
+        }
+
+        $this->requireLogin();
+        $userId = $this->auth_manager->getUserId();
+
+        if (!($this->auth_manager->admin()->doesUserHaveRole($userId, $role))) {
+            $view = new VStatus();
+            $view->showStatus(403);
+            exit;
+        }
+    }
+
+    public function doesUserHaveRole(string $role): bool
+    {
+        $validRoles = (new \ReflectionClass(Roles::class))->getConstants();
+
+        if (!in_array($role, $validRoles, true)) {
+            throw new InvalidArgumentException('Invalid role: ' . $role . 'passed to requireRole()');
+        }
+
+        $this->requireLogin();
+        $userId = $this->auth_manager->getUserId();
+
+        if (!($this->auth_manager->admin()->doesUserHaveRole($userId, $role))) {
+            return false;
+        }
+
+        return true;
     }
 }
